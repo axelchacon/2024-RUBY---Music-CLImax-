2b378c1f49120f1749fa234b7aab41c59b305726
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-02-16 10:51:11 -0500

    Ya terminado 1 h que es paso1

--- ejemplos.rb
+++ ejemplos.rb
@@ -18,18 +18,102 @@
 # puts ruby_objeto1['edad']   # Imprime 25
 # puts ruby_objeto1['ciudad'] # Imprime "Ciudad de Ejemplo"
 
+# ####################      Ejemplo 2      ###################################################
 
+# require "json"
+
+# # store = JSON.parse(file.read('store.json'), symbolize_names:true)
+# # pp File.read('store.json')
+# # pp JSON.parse(File.read('store.json')) #read hace la función de abrirlo, leerlo y cerrar el archcivo
+# store = JSON.parse(File.read('store.json'), symbolize_names:true) #Cambiar de JSON a hash con símboloso
+# # p store
+# # pp store
+# # p store[0][:name]
+# # menu = "create | sow ID | update ID | delete ID | exit"
+# # puts menu 
+# # print "> "
+# # action = gets.chomp
+# # puts action
+# class Playlist
+#     def initialize(id:, name:, description:, songs:)
+#         @id = id
+#         @name = name
+#         @description = description
+#         @songs = songs
+#     end 
+# end 
+
+# playlists = store.map do |playlist|
+#     Playlist.new(id: playlist[:id], name: playlist[:name], description: playlist[:description], songs: playlist[:songs])
+# end
+
+# ####################      Ejemplo 3     ###################################################
+# require "json"
+# store = JSON.parse(File.read('store.json'), symbolize_names:true) #Cambiar de JSON a hash con símboloso
+# class Playlist
+#     def initialize(id:, name:, description:, songs:)
+#         @id = id
+#         @name = name
+#         @description = description
+#         @songs = songs
+#     end 
+# end 
+
+# playlists = store.map do |playlist|
+#     Playlist.new(id: playlist[:id], name: playlist[:name], description: playlist[:description], songs: playlist[:songs])
+# end
+# pp playlists
+
+
+# ####################      Ejemplo 4     ###################################################
+# require "json"
+
+# store = JSON.parse(File.read('store.json'), symbolize_names: true)
+
+# class Song
+#   def initialize(id, title, artists, album, released)
+#     @id = id
+#     @title = title
+#     @artists = artists
+#     @album = album
+#     @released = released
+#   end
+# end
+
+# class Playlist
+#   def initialize(id, name, description, songs)
+#     @id = id
+#     @name = name
+#     @description = description
+#     @songs = songs.map { |song| Song.new(song[:id], song[:title], song[:artists], song[:album], song[:released]) }
+#   end
+# end
+
+# playlists = store.map do |playlist|
+#   Playlist.new(playlist[:id], playlist[:name], playlist[:description], playlist[:songs])
+# end
+
+# pp playlists
+
+
+
+# ####################      Ejemplo 5     ###################################################
 require "json"
 
-# store = JSON.parse(file.read('store.json'), symbolize_names:true)
-pp File.read('store.json')
-pp JSON.parse(File.read('store.json')) #read hace la función de abrirlo, leerlo y cerrar el archcivo
-store = JSON.parse(File.read('store.json'), symbolize_names:true) #Cambiar de JSON a hash con símboloso
-p store
-pp store
-p store[0][:name]
-menu = "create | sow ID | update ID | delete ID | exit"
-puts menu 
-print "> "
-action = gets.chomp
-puts action
+store = JSON.parse(File.read('store.json'), symbolize_names: true)
+
+
+class Playlist
+  def initialize(id, name, description, songs)
+    @id = id
+    @name = name
+    @description = description
+    @songs = songs
+  end
+end
+
+playlists = store.map do |playlist|
+  Playlist.new(playlist[:id], playlist[:name], playlist[:description], playlist[:songs])
+end
+
+pp playlists
--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 require "json"
+require_relative "playlist"
 
 # store = JSON.parse(file.read('store.json'), symbolize_names:true)
 pp File.read('store.json')
 pp JSON.parse(File.read('store.json')) #read hace la función de abrirlo, leerlo y cerrar el archcivo
 store = JSON.parse(File.read('store.json'), symbolize_names:true) #Cambiar de JSON a hash con símboloso
-p store
-pp store
-p store[0][:name]
+# p store
+# pp store
+# p store[0][:name]
 
 playlists = store.map do |playlist|
     Playlist.new(id: playlist[:id], name: playlist[:name], description: playlist[:description], songs: playlist[:songs])
@@ -17,3 +18,5 @@ puts menu
 print "> "
 action = gets.chomp
 puts action
+
+pp playlists
